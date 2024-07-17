582248b0aa1de15f879d2bbb587737638c8ea352
slayeh17 <debakroy7@gmail.com>
2024-07-17 12:58:46 +0530

    add turn off canonical mode... input is now taken byte by byte rather line by line

--- kilo.c
+++ kilo.c
@@ -1,6 +1,30 @@
+#include <stdlib.h>
+#include <termios.h>
 #include <unistd.h>
+
+struct termios orig_termios;
+
+void disableRawMode() {
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+// this function does not prints whatever we are typing in the terminal
+// We are just turning off the ECHO feature
+void enableRawMode() {
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disableRawMode);
+
+    //here the orig_termios value is being modified by copying the value in raw. Then we just & with the NOT of ECHO
+    struct termios raw = orig_termios;
+    raw.c_lflag &= ~(ECHO | ICANON);
+
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
 int main() {
+    enableRawMode();
+
 	char c;
-	while (read(STDIN_FILENO, &c, 1) == 1);
+	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
 	return 0;
 }
